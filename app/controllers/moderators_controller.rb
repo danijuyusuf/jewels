d4b84a576b759c68698aa3d7e4ade44c41920bd1
andela-ydaniju <danijuyusuf@gmail.com>
2016-05-04 00:30:38 +0100

    create dashboar

--- app/controllers/moderators_controller.rb
+++ app/controllers/moderators_controller.rb
@@ -2,4 +2,32 @@ class ModeratorsController < ApplicationController
   def new
     @moderator = Moderator.new
   end
+
+  def create
+    @moderator = Moderator.new(moderator_params)
+    if @moderator.save
+      flash[:success] = "Moderator #{@moderator.firstname} created!"
+      redirect_to @moderator
+    else
+      flash[:error] = "An error occured in creatin Moderator"
+      render "new"
+    end
+  end
+
+  def show
+    @moderator = Moderator.find_by(id: params[:id])
+  end
+end
+
+private
+
+def moderator_params
+  params.require(:moderator).permit(
+    :firstname,
+    :lastname,
+    :email,
+    :password,
+    :password_confirmation,
+    :super
+  )
 end
